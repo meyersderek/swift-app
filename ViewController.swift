c07557a5a80221ec765ee906878ebdfa879717b1
Derek Meyers <s.derekmeyers@gmail.com>
2017-04-06 08:08:11 -0400

    My first commit.

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Derek Meyers on 3/18/17.
+//  Copyright © 2017 AppAlchemy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var nameLabel: UILabel!
+    
+    var tapCount = 0
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            nameLabel.text = "You tapped the button 10 times!"
+        }
+        print(tapCount)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
